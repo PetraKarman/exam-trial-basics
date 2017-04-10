1a1f3736e7be80056ff8d7684f2dd82dbe16724b
Petra Karman <karmanpetra@gmail.com>
2017-04-10 17:13:17 +0200

    grammar nazi

--- src/CountAs.java
+++ src/CountAs.java
@@ -1,5 +1,5 @@
 // Create a function that takes a filename as string parameter,
-// counts the occurances of the letter "a", and returns it as a number.
+// counts the occurrences of the letter "a", and returns it as a number.
 // If the file does not exist, the function should return 0 and not break.
 
 // example: on the input "afile.txt" the function should return 28 - print this result
